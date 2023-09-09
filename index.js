a24b0664e8469a83057be7fcd91cdf1965a8cf4d
Ayça <46596708+aycaoktay@users.noreply.github.com>
2023-09-09 18:47:57 +0300

    Fixing file format

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+const data = [
+  { id: 1, name: "iphone 14", price: "30000 TL", isActive: "true" , imageUrl : "1.jpg" },
+  { id: 2, name: "iphone 15", price: "40000 TL", isActive: "false", imageUrl : "2.jpg" },
+  { id: 3, name: "iphone 16", price: "50000 TL", isActive: "true" , imageUrl : "3.jpg"},
+  { id: 4, name: "iphone 17", price: "60000 TL", isActive: "true" , imageUrl : "1.jpg"},
+];
+
+//routes
+app.use("/products/:id", function (req, res) {
+  const urun = data.find(u => u.id == req.params.id);
+  res.render("product-details" , urun);
+});
+app.use("/products", function (req, res) {
+  res.render("products", {
+    urunler: data,
+  });
+});
+app.use("/", function (req, res) {
+  res.render("index");
+});
+app.listen(3000, () => {
+  console.log("listenin at port 3000");
+});
